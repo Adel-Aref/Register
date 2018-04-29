8d7f94f0103019d8f9b9eab46aaa2b13c94a4893
Adel <adel.aref16@gmail.com>
2018-04-29 13:57:02 -0700

    first commit

---
+++ Register/MyController.swift
@@ -0,0 +1,21 @@
+//
+//  MyController.swift
+//  Register
+//
+//  Created by Have a Mission on 3/19/18.
+//  Copyright © 2018 Have a Mission. All rights reserved.
+//
+
+import UIKit
+
+class MyController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
